c52c39b3ae0de203b9a2ba916d16c850c1874e25
clavei1-admin <pokroy@clavei.es>
2024-07-06 22:21:21 +0200

    edit

---
+++ main.tf
@@ -0,0 +1,9 @@
+module "network" {
+  source              = "./ modules/network"  # or use a Git URL or Terraform Registry URL
+  vnet_name           = var.vnet_name
+  address_space       = var.address_space
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg-pokroy-tf-demo-01.name
+  subnet_name         = var.subnet_name
+  subnet_prefixes     = var.subnet_prefixes
+}
--- variables.tf
+++ variables.tf
@@ -12,3 +12,34 @@ variable "environment"{
 variable "project"{
     type = string
 }
+
+
+variable "location" {
+  description = "The location of the resources"
+  type        = string
+  default     = "westeurope"
+}
+
+variable "vnet_name" {
+  description = "The name of the virtual network"
+  type        = string
+  default     = "example-vnet"
+}
+
+variable "address_space" {
+  description = "The address space of the virtual network"
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
+
+variable "subnet_name" {
+  description = "The name of the subnet"
+  type        = string
+  default     = "example-subnet"
+}
+
+variable "subnet_prefixes" {
+  description = "The address prefixes for the subnet"
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+}
