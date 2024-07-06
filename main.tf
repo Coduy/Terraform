c01b8148e5becfb7423707c0d26fee12f35188ab
clavei1-admin <pokroy@clavei.es>
2024-07-06 22:28:46 +0200

    edit

--- main.tf
+++ main.tf
@@ -7,3 +7,14 @@ module "network" {
   subnet_name         = var.subnet_name
   subnet_prefixes     = var.subnet_prefixes
 }
+
+
+module "network" {
+  source              = "./modules/network"  # or use a Git URL or Terraform Registry URL
+  vnet_name           = "vamos-test-net"
+  address_space       = ["192.168.0.0/16"]
+  location            = "WestEurope"
+  resource_group_name = azurerm_resource_group.rg-pokroy-tf-demo-01.name
+  subnet_name         = "subnet-peuqeño"
+  subnet_prefixes     = ["192.168.0.0/24"]
+}
