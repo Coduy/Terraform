974fe6f6179152b6be3d0dee589c504b245d69a2
clavei1-admin <pokroy@clavei.es>
2024-07-07 21:52:28 +0200

    add a simple template

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ data "template_file" "storage_config"{
 resource "azurerm_storage_account" "example" {
   name                     = var.storage_name
   resource_group_name      = azurerm_resource_group.rg-pokroy-tf-demo-01.name
-  location                 = azurerm_resource_group.var.region
+  location                 = var.region
   account_tier             = var.storage_tier
   account_replication_type = var.replication_type
 
