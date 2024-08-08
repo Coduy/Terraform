c463f3f049029f46d519963504f05ea7cdc24033
clavei1-admin <pokroy@clavei.es>
2024-08-08 11:17:18 +0200

    vamos

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,4 +1,10 @@
 # modules/virtual_network/outputs.tf
-output "vnet_id" {
-  value = azurerm_virtual_network.vnet.id
+
+output "resource_group_name" {
+  value = azurerm_virtual_network.vnet.resource_group_name
+}
+
+
+output "vnet_name" {
+  value = azurerm_virtual_network.vnet.name
 }
