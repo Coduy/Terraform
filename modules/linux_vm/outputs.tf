b35c6912f635955d2733c163cb0ad13e7ab74f50
clavei1-admin <pokroy@clavei.es>
2024-08-08 13:15:15 +0200

    vamos

--- modules/linux_vm/outputs.tf
+++ modules/linux_vm/outputs.tf
@@ -1,7 +1,3 @@
 output "admin_username" {
   value = azurerm_linux_virtual_machine.vm.admin_username
 }
-
-output "public_ip_address" {
-  value = azurerm_network_interface.nic.private_ip_address
-}
