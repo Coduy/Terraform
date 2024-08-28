4f66f7e49898527bf9d743a9679946048fef7b0f
Pawel Okroy <pokroy@clavei.es>
2024-08-28 11:49:59 +0200

    chore

--- main.tf
+++ main.tf
@@ -71,6 +71,7 @@ module "vnet-1" {
   resource_group_name = azurerm_resource_group.rg-pokroy-tf-demo-01.name
 }
 
+
 module "subnet-1" {
   source                = "./modules/subnet"
   subnet_name           = "subnet1"
