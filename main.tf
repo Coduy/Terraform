4bfee23eaad14962788d692921e010f0c4d3367a
Pawel Okroy <pokroy@clavei.es>
2024-08-28 12:30:51 +0200

    chore

--- main.tf
+++ main.tf
@@ -41,7 +41,6 @@ module "linux_vm" {
   network_interface_ids = [
     module.nic.network_interface_id
   ]
-
 }
 
 # Public IP Module
@@ -78,4 +77,33 @@ module "subnet-3" {
   resource_group_name   = module.vnet-3.resource_group_name
   virtual_network_name  = module.vnet-3.vnet_name
   subnet_prefixes       = var.subnet3_prefixes
+}
+
+# NIC Module
+module "nic-3" {
+  source                = "./modules/nic"
+  name                  = "nic-3"
+  location              = "West Europe"
+  resource_group_name   = azurerm_resource_group.rg-pokroy-tf-demo-01.name
+  subnet_id             = module.subnet-3.subnet_id
+  private_ip_allocation = "Dynamic"
+  public_ip_address_id  = null #module.public_ip.public_ip_id
+  nsg_id = var.NSG_NIC_LINK ? module.nsg.nsg_id : null
+}
+
+
+# Linux VM Module
+module "linux_vm_3" {
+  create_resource     = var.create_resource
+  source              = "./modules/linux_vm"
+  vm_name             = "my-linux-vm-03"
+  location            = azurerm_resource_group.rg-pokroy-tf-demo-01.location
+  resource_group_name = azurerm_resource_group.rg-pokroy-tf-demo-01.name
+  subnet_id           = module.subnet-3.subnet_id
+  vm_size             = "Standard_B1s"
+  admin_username      = "adminuser"
+  ssh_public_key      = file("~/.ssh/id_rsa.pub")  # Path to your SSH public key
+  network_interface_ids = [
+    module.nic-3.network_interface_id
+  ]
 }
