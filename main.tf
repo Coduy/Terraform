b44890fc64d1afe7a3f98fb3b9fa5c5ddc91a559
clavei1-admin <pokroy@clavei.es>
2024-08-30 11:03:43 +0200

    vamos

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ resource "azurerm_network_interface_security_group_association" "example" {
 module "container_app" {
   source              = "./modules/container_app"
   name                = "my-container-app"
-  resource_group_name = azurerm_resource_group.rg-pokroy-tf-demo-01.location
+  resource_group_name = azurerm_resource_group.rg-pokroy-tf-demo-01.name
   container_image     = "nginx:latest"
   environment_id      = module.container_app_env.id
 
