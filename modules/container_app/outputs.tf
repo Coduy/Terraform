4f2fc3847460c587b107ba3a4d0cf1cd1c568260
clavei1-admin <pokroy@clavei.es>
2024-08-30 10:27:18 +0200

    add module

--- modules/container_app/outputs.tf
+++ modules/container_app/outputs.tf
@@ -3,7 +3,3 @@ output "id" {
   value       = azurerm_container_app.this.id
 }
 
-output "fqdn" {
-  description = "The FQDN of the Container App"
-  value       = azurerm_container_app.this.default_domain_name
-}
