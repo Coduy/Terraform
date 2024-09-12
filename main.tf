a8eaf8fea6a57818b57ed7e5872da71d29457386
clavei1-admin <pokroy@clavei.es>
2024-09-12 09:47:32 +0200

    chore

--- main.tf
+++ main.tf
@@ -1,14 +1,4 @@
 
 
 
-module "container_app_env" {
-  source              = "./modules/container_app_env"
-  name                = "containerappenv"
-  location            = "East US" #azurerm_resource_group.rg-pokroy-tf-demo-01.location
-  resource_group_name = azurerm_resource_group.rg-pokroy-tf-demo-01.name
-  tags = {
-    enviroment = "testing"
-    owner      = "pokroy"
-  }
-}
 
