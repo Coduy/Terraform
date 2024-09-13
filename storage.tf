720f87647b541d8b4bac9ceb6dffdf149aa2b78c
clavei1-admin <pokroy@clavei.es>
2024-09-13 08:51:46 +0200

    add storage module

--- storage.tf
+++ storage.tf
@@ -2,7 +2,7 @@ module "storage_account" {
   source              = "./modules/storage"
   resource_group_name = "rg-pokroy-tf-demo-03"
   location            = "West Europe"
-  storage_account_name = "mystorageaccountunique123"
+  storage_account_name = "pvcshare"
   fileshare_name      = "myfileshare"
   fileshare_quota     = 10
   tags = {
