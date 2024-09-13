5a591e75480f754512740a9d65eff48d51b3de6d
clavei1-admin <pokroy@clavei.es>
2024-09-13 09:11:03 +0200

    add storage module

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -2,11 +2,6 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
 resource "azurerm_storage_account" "storage" {
   name                     = var.storage_account_name
   resource_group_name       = azurerm_resource_group.rg.name
--- modules/storage/variables.tf
+++ modules/storage/variables.tf
@@ -1,8 +1,3 @@
-variable "resource_group_name" {
-  description = "The name of the resource group in which to create the storage account."
-  type        = string
-}
-
 variable "location" {
   description = "The Azure region in which to create resources."
   type        = string
