e08c16cd7f006aff3205fcaa254322a10cffbda5
clavei1-admin <pokroy@clavei.es>
2024-09-15 21:44:10 +0200

    add modules, chore

--- modules/kubernetes_cluster/main.tf
+++ modules/kubernetes_cluster/main.tf
@@ -28,4 +28,13 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   }
 }
 
+terraform {
+  required_providers {
+    azapi = {
+      source  = "Azure/azapi"
+      version = "=0.4.0"
+    }
+  }
+}
+
 
