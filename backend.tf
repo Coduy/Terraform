ab80308d12b440ebba8f2b62c3817f9a1966cd30
clavei1-admin <pokroy@clavei.es>
2024-09-15 21:21:20 +0200

    add modules, chore

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   required_version = ">=1.0"
-
+  
   required_providers {
     azapi = {
       source  = "azure/azapi"
@@ -9,14 +9,6 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~>3.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
-    }
-    time = {
-      source  = "hashicorp/time"
-      version = "0.9.1"
     }
   }
 }
