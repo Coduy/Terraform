3792fe4d9382e05894b1d1c6af63a7ae18c97438
clavei1-admin <pokroy@clavei.es>
2024-09-15 21:15:16 +0200

    add modules, chore

---
+++ modules/kubernetes_cluster/variables.tf
@@ -0,0 +1,46 @@
+variable "cluster_name" {
+  type        = string
+  description = "Name of the Kubernetes cluster"
+}
+
+variable "location" {
+  type        = string
+  description = "Location of the Kubernetes cluster"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group name"
+}
+
+variable "dns_prefix" {
+  type        = string
+  description = "DNS prefix for the Kubernetes cluster"
+}
+
+variable "vm_size" {
+  type        = string
+  description = "VM size for the nodes"
+  default     = "Standard_B2ms"
+}
+
+variable "node_count" {
+  type        = number
+  description = "Number of nodes in the cluster"
+  default     = 1
+}
+
+variable "admin_username" {
+  type        = string
+  description = "Admin username for Linux VM"
+}
+
+variable "ssh_key" {
+  type        = string
+  description = "SSH public key"
+}
+
+variable "node_pool_name" {
+  description = "The name of the default node pool in the AKS cluster"
+  type        = string
+}
---
+++ modules/ssh_key/variables.tf
@@ -0,0 +1,14 @@
+variable "ssh_key_name" {
+  type        = string
+  description = "Name for the SSH key"
+}
+
+variable "location" {
+  type        = string
+  description = "Location for the SSH key"
+}
+
+variable "resource_group_id" {
+  type        = string
+  description = "Resource group ID where the SSH key will be created"
+}
